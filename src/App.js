c911c046c95a03b546c6ee7fa80a8dc37ec05bbc
Shane-Geary <shane.geary@glowstik.com>
2023-02-22 09:38:15 -0700

    Minor - Testing background_mode for reading geo location when window has been closed on IOS

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import {useEffect} from 'react'
+
 import './App.css'
 
 import Navigation from './Navigation'
 
 function App() {
+
+  useEffect(() => {
+    navigator.serviceWorker.ready.then(registration => {
+      registration.periodicSync.register('my-sync', {
+        minInterval: 10 * 10 * 1000,
+      })
+    })    
+  }, [])
 
   return (
     <div className="App">
