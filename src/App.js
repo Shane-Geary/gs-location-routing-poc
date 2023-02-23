be45b7a9c828a26118c11725638be615538792d0
Shane-Geary <shane.geary@glowstik.com>
2023-02-23 09:41:09 -0700

    Minor - Removed unused code

--- src/App.js
+++ src/App.js
@@ -1,18 +1,8 @@
-import {useEffect} from 'react'
-
 import './App.css'
 
 import Navigation from './Navigation'
 
 function App() {
-
-  // useEffect(() => {
-  //   navigator.serviceWorker.ready.then(registration => {
-  //     registration.periodicSync.register('my-sync', {
-  //       minInterval: 10 * 10 * 1000,
-  //     })
-  //   })    
-  // }, [])
 
   return (
     <div className="App">
