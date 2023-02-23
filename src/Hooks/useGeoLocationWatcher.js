4c9197b9f88b96fa707f6c4496eabf6aa927efe6
Shane-Geary <shane.geary@glowstik.com>
2023-02-23 10:00:39 -0700

    Minor - More detailed commenting on useGeoLocationWatcher hook

--- src/Hooks/useGeoLocationWatcher.js
+++ src/Hooks/useGeoLocationWatcher.js
@@ -13,6 +13,7 @@ export const useGeoLocationWatcher = (startGeoWatch) => {
 
     const navigate = useNavigate()
 
+    // State variables for the geolocation watch ID and interval ID
     const [geoWatchID, setGeoWatchID] = useState(null)
     const [id, setId] = useState(null)
 
@@ -20,7 +21,9 @@ export const useGeoLocationWatcher = (startGeoWatch) => {
         // Function that runs at a regular interval to watch for the user's location.
         let counter = 0
         const geoWatchTimer = () => {
+            // Watch for the user's position using the Geolocation API.
             const watchID = navigator.geolocation.watchPosition(
+                // On success, set coordinates to geoWatchID state, navigate to map, clear watch and reset counter to 0
                 (success) => {
                     console.log(success)
                     setGeoWatchID(success)
@@ -29,6 +32,7 @@ export const useGeoLocationWatcher = (startGeoWatch) => {
                     navigator.geolocation.clearWatch(watchID)
                     counter = 0
                 },
+                // On error, set geoWatchID back to null, navigate to LocationPermissions, clear watch and reset counter to 0
                 (error) => {
                     console.log(error)
                     setGeoWatchID(null)
@@ -38,10 +42,12 @@ export const useGeoLocationWatcher = (startGeoWatch) => {
                 }
             )
             console.log('geo watch tick: ', watchID)
+            // This will keep track of the number of calls made to watch geolocation
             setId(watchID)
 
             counter++
 
+            // If no response is received after 5 attempts, redirect to the location permissions page.
             if(counter >= 5) {
                 console.log('error: no response from geolocation')
                 navigate('/locationpermissions')
@@ -50,11 +56,13 @@ export const useGeoLocationWatcher = (startGeoWatch) => {
             }
         }
         let watchIDInterval = null
+        // Start the geolocation watch interval if startGeoWatch is true.
         if(startGeoWatch) {
             watchIDInterval = setInterval(geoWatchTimer, 5000)
         }
         console.log(watchIDInterval)
 
+        // Clear the interval and the geolocation watch on unmount.
         return () => {
             clearInterval(watchIDInterval)
             navigator.geolocation.clearWatch(id)
@@ -63,5 +71,6 @@ export const useGeoLocationWatcher = (startGeoWatch) => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [startGeoWatch])
 
+    // Return an object with the current values of the geolocation watch and interval IDs
     return {geoWatchID, id}
 }
