586edd7df0d4f764cd213ec3f71164f900dc9b14
Shane-Geary <shane.geary@glowstik.com>
2023-02-24 12:58:45 -0700

    Minor - Removed unused code in useGeoLocationRequest hook

--- src/Hooks/useGeoLocationRequest.js
+++ src/Hooks/useGeoLocationRequest.js
@@ -1,13 +1,9 @@
-import {useNavigate} from "react-router-dom"
-
 /**
 Custom React hook for requesting geolocation permission and navigation
 * @returns {Function} - A function that when invoked, prompts the user for geolocation permission and navigates to either the Glowstik map or the location permissions page based on the permission status.
 */
 
 export const useGeoLocationRequest = (onSuccess, onError) => {
-
-    const navigate = useNavigate()
 
     // Prompts the user for geolocation and navigates to Glowstik map if successful or location permissions page if not
     const locationRequest = async () => {
